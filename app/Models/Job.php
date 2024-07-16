12de1bff25f656bb7c88cf15c858c0197fae9347
kapakrystian <56194438+kapakrystian@users.noreply.github.com>
2024-07-16 20:48:58 +0200

    Factories. JobFactory, EmployerFactory.

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -3,11 +3,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 
 class Job extends Model
 {
+    use HasFactory; 
+
     //odwołanie do konkretnej tabeli w bazie danych, którą reprezentuje model
     protected $table = 'job_listing';
 
