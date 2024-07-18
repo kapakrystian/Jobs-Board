aff9f2846bfb58ff2c13e25eb55e0b9ecf850d9f
kapakrystian <56194438+kapakrystian@users.noreply.github.com>
2024-07-18 19:49:51 +0200

    Eager loading for employers, new look for a single job advertisement.

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -3,14 +3,14 @@
         Job Listing
     </x-slot:heading>
 
-    <ul>
-    @foreach ($jobs as $job)
-    <li>
-        <a href="/jobs/{{$job['id']}}" class="text-blue-500 hover:underline">
-        <strong>{{ $job['title'] }}:</strong> Pays {{ $job['salary'] }} per month.
-        </a>
-    </li>
-    @endforeach
-    </ul>
-
+    <div class="space-y-4">
+        @foreach ($jobs as $job)
+            <a href="/jobs/{{$job['id']}}" class="block px-4 py-6 border border-gray-200 rounded-lg">
+                <div class="font-bold text-blue-500 text-sm">{{$job->employer->name}}</div>
+                <div>
+                    <strong>{{ $job['title'] }}:</strong> Pays {{ $job['salary'] }} per month.
+                </div>
+            </a>
+        @endforeach
+    </div>
 </x-layout>
