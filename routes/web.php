a551c7be0eb35d87700f52d92f50cf25dc1b4343
kapakrystian <56194438+kapakrystian@users.noreply.github.com>
2024-07-19 09:42:53 +0200

    Pagination

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@ Route::get('/', function () {
 });
 
 Route::get('/jobs', function () {
-    $jobs = Job::with('employer')->get();
+    $jobs = Job::with('employer')->cursorPaginate(perPage: 5);
     return view('jobs', [
         'jobs' => $jobs
     ]);
